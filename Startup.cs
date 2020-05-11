caac68487eacdd91200943451f5f09405aa7e9ff
Johnny Barrantes <jbarrantes@gmail.com>
2020-05-11 12:42:18 -0600

    se agrega el listado de terneros

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace cowsystem
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Terneros}/{action=Index}/{id?}");
             });
         }
     }
