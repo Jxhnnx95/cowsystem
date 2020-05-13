ea76f3933dea394f2d668b282c7ef157a60e33e1
Johnny Barrantes <jbarrantes@gmail.com>
2020-05-13 08:47:06 -0600

    se agrega utilitario de tiempo relativo

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var now = moment("20111031", "YYYYMMDD").fromNow();
+var date = $('.actualizar');
+date.innerHTML = now;
